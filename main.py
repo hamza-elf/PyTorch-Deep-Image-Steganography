ee50dd54a94f87ac550a904c2c592dd41581b9ff
hamza-elf <elfaidihamza@gmail.com>
2021-06-10 16:54:48 +0100

    Entraînement du modèle
    Co-Authored-By: AchrafDer <20994890+Melvero@users.noreply.github.com>

--- main.py
+++ main.py
@@ -11,28 +11,20 @@
 
 import argparse
 import os
-import shutil
-import socket
 import time
 
 import torch
-import torch.backends.cudnn as cudnn
 import torch.nn as nn
 import torch.nn.parallel
-import torch.optim as optim
 import torch.utils.data
 import torchvision.utils as vutils
-from tensorboardX import SummaryWriter
 from torch.autograd import Variable
-from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader
 
 import utils.transformed as transforms
 from data.ImageFolderDataset import MyImageFolder
 from models.HidingUNet import UnetGenerator
 from models.RevealNet import RevealNet
-
-DATA_DIR = '/n/liyz/data/deep-steganography-dataset/'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', default="train",
@@ -76,7 +68,6 @@ parser.add_argument('--beta', type=float, default=0.75,
 parser.add_argument('--remark', default='', help='comment')
 parser.add_argument('--test', default='', help='test mode, you need give the test pics dirs in this param')
 
-parser.add_argument('--hostname', default=socket.gethostname(), help='the  host name of the running server')
 parser.add_argument('--debug', type=bool, default=False, help='debug mode do not create folders')
 parser.add_argument('--logFrequency', type=int, default=10, help='the frequency of print the log on the console')
 parser.add_argument('--resultPicFrequency', type=int, default=100, help='the frequency of save the resultPic')
@@ -108,231 +99,41 @@ def main():
     #################  output configuration   ###############
     opt = parser.parse_args()
     logPath = opt.outlogs + '/%s_%d_log.txt' % (opt.dataset, opt.batchSize)
+    opt.Hnet = "./checkPoint/netH_epoch_73,sumloss=0.000447,Hloss=0.000258.pth"
+    opt.Rnet = "./checkPoint/netR_epoch_73,sumloss=0.000447,Rloss=0.000252.pth"
+    testdir = opt.test
+    test_dataset = MyImageFolder(
+        testdir,
+        transforms.Compose([
+            transforms.Resize([opt.imageSize, opt.imageSize]),
+            transforms.ToTensor(),
+        ]))
+    assert test_dataset
 
     if torch.cuda.is_available():
-        map_location=lambda storage, loc: storage.cuda()
+        map_location = lambda storage, loc: storage.cuda()
     else:
-        map_location='cpu'
+        map_location = 'cpu'
 
     Hnet = UnetGenerator(input_nc=6, output_nc=3, num_downs=7, output_function=nn.Sigmoid)
-#     Hnet.cuda()
     Hnet.apply(weights_init)
-    # whether to load pre-trained model
-    if opt.Hnet != "":
-        Hnet.load_state_dict(torch.load(opt.Hnet,map_location=map_location))
-    if opt.ngpu > 1:
-        Hnet = torch.nn.DataParallel(Hnet).cuda()
+    Hnet.load_state_dict(torch.load(opt.Hnet, map_location=map_location))
     print_network(Hnet)
 
     Rnet = RevealNet(output_function=nn.Sigmoid)
-#     Rnet.cuda()
     Rnet.apply(weights_init)
-    if opt.Rnet != '':
-        Rnet.load_state_dict(torch.load(opt.Rnet,map_location=map_location))
-    if opt.ngpu > 1:
-        Rnet = torch.nn.DataParallel(Rnet).cuda()
+    Rnet.load_state_dict(torch.load(opt.Rnet, map_location=map_location))
     print_network(Rnet)
 
     # MSE loss
-#     criterion = nn.MSELoss().cuda()
+    #     criterion = nn.MSELoss().cuda()
     criterion = nn.MSELoss()
-    # training mode
-    if opt.test == '':
-        # setup optimizer
-        optimizerH = optim.Adam(Hnet.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-        schedulerH = ReduceLROnPlateau(optimizerH, mode='min', factor=0.2, patience=5, verbose=True)
-
-        optimizerR = optim.Adam(Rnet.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-        schedulerR = ReduceLROnPlateau(optimizerR, mode='min', factor=0.2, patience=8, verbose=True)
-
-        train_loader = DataLoader(train_dataset, batch_size=opt.batchSize,
-                                  shuffle=True, num_workers=int(opt.workers))
-        val_loader = DataLoader(val_dataset, batch_size=opt.batchSize,
-                                shuffle=False, num_workers=int(opt.workers))
-        smallestLoss = 10000
-        print_log("training is beginning .......................................................", logPath)
-        for epoch in range(opt.niter):
-            ######################## train ##########################################
-            train(train_loader, epoch, Hnet=Hnet, Rnet=Rnet, criterion=criterion)
-
-            ####################### validation  #####################################
-            val_hloss, val_rloss, val_sumloss = validation(val_loader, epoch, Hnet=Hnet, Rnet=Rnet, criterion=criterion)
-
-            ####################### adjust learning rate ############################
-            schedulerH.step(val_sumloss)
-            schedulerR.step(val_rloss)
-
-            # save the best model parameters
-            if val_sumloss < globals()["smallestLoss"]:
-                globals()["smallestLoss"] = val_sumloss
-                # do checkPointing
-                torch.save(Hnet.state_dict(),
-                           '%s/netH_epoch_%d,sumloss=%.6f,Hloss=%.6f.pth' % (
-                               opt.outckpts, epoch, val_sumloss, val_hloss))
-                torch.save(Rnet.state_dict(),
-                           '%s/netR_epoch_%d,sumloss=%.6f,Rloss=%.6f.pth' % (
-                               opt.outckpts, epoch, val_sumloss, val_rloss))
-
-        writer.close()
-
     # test mode
-    else:
-        test_loader = DataLoader(test_dataset, batch_size=opt.batchSize,
-                                 shuffle=False, num_workers=int(opt.workers))
-        test(test_loader, 0, Hnet=Hnet, Rnet=Rnet, criterion=criterion)
-        print("##################   test is completed, the result pic is saved in the ./training/yourcompuer+time/testPics/   ######################")
-
-
-def train(train_loader, epoch, Hnet, Rnet, criterion):
-    batch_time = AverageMeter()
-    data_time = AverageMeter()
-    Hlosses = AverageMeter()  # record loss of H-net
-    Rlosses = AverageMeter()  # record loss of R-net
-    SumLosses = AverageMeter()  # record Hloss + β*Rloss
-
-    # switch to train mode
-    Hnet.train()
-    Rnet.train()
-
-    start_time = time.time()
-    for i, data in enumerate(train_loader, 0):
-        data_time.update(time.time() - start_time)
-
-        Hnet.zero_grad()
-        Rnet.zero_grad()
-
-        all_pics = data  # allpics contains cover images and secret images
-        this_batch_size = int(all_pics.size()[0] / 2)  # get true batch size of this step 
-
-        # first half of images will become cover images, the rest are treated as secret images
-        cover_img = all_pics[0:this_batch_size, :, :, :]  # batchsize,3,256,256
-        secret_img = all_pics[this_batch_size:this_batch_size * 2, :, :, :]
-
-        # concat cover images and secret images as input of H-net
-        concat_img = torch.cat([cover_img, secret_img], dim=1)
-
-        if opt.cuda:
-            cover_img = cover_img.cuda()
-            secret_img = secret_img.cuda()
-            concat_img = concat_img.cuda()
-
-        concat_imgv = Variable(concat_img)
-        cover_imgv = Variable(cover_img)
-
-        container_img = Hnet(concat_imgv)  # put concat_image into H-net and get container image
-        errH = criterion(container_img, cover_imgv)  # loss between cover and container
-        Hlosses.update(errH.data[0], this_batch_size)
-
-        rev_secret_img = Rnet(container_img)  # put concatenated image into R-net and get revealed secret image
-        secret_imgv = Variable(secret_img)
-        errR = criterion(rev_secret_img, secret_imgv)  # loss between secret image and revealed secret image
-        Rlosses.update(errR.data[0], this_batch_size)
-
-        betaerrR_secret = opt.beta * errR
-        err_sum = errH + betaerrR_secret
-        SumLosses.update(err_sum.data[0], this_batch_size)
-
-        err_sum.backward()
-
-        optimizerH.step()
-        optimizerR.step()
-
-        batch_time.update(time.time() - start_time)
-        start_time = time.time()
-
-        log = '[%d/%d][%d/%d]\tLoss_H: %.4f Loss_R: %.4f Loss_sum: %.4f \tdatatime: %.4f \tbatchtime: %.4f' % (
-            epoch, opt.niter, i, len(train_loader),
-            Hlosses.val, Rlosses.val, SumLosses.val, data_time.val, batch_time.val)
-
-        if i % opt.logFrequency == 0:
-            print_log(log, logPath)
-        else:
-            print_log(log, logPath, console=False)
-
-        # genereate a picture every resultPicFrequency steps
-        if epoch % 1 == 0 and i % opt.resultPicFrequency == 0:
-            save_result_pic(this_batch_size, cover_img, container_img.data, secret_img, rev_secret_img.data, epoch, i,
-                            opt.trainpics)
-
-    # epcoh log
-    epoch_log = "one epoch time is %.4f======================================================================" % (
-        batch_time.sum) + "\n"
-    epoch_log = epoch_log + "epoch learning rate: optimizerH_lr = %.8f      optimizerR_lr = %.8f" % (
-        optimizerH.param_groups[0]['lr'], optimizerR.param_groups[0]['lr']) + "\n"
-    epoch_log = epoch_log + "epoch_Hloss=%.6f\tepoch_Rloss=%.6f\tepoch_sumLoss=%.6f" % (
-        Hlosses.avg, Rlosses.avg, SumLosses.avg)
-    print_log(epoch_log, logPath)
-
-
-    if not opt.debug:
-        # record lr
-        writer.add_scalar("lr/H_lr", optimizerH.param_groups[0]['lr'], epoch)
-        writer.add_scalar("lr/R_lr", optimizerR.param_groups[0]['lr'], epoch)
-        writer.add_scalar("lr/beta", opt.beta, epoch)
-        # record loss
-        writer.add_scalar('train/R_loss', Rlosses.avg, epoch)
-        writer.add_scalar('train/H_loss', Hlosses.avg, epoch)
-        writer.add_scalar('train/sum_loss', SumLosses.avg, epoch)
-
-
-def validation(val_loader, epoch, Hnet, Rnet, criterion):
+    test_loader = DataLoader(test_dataset, batch_size=opt.batchSize,
+                             shuffle=False, num_workers=int(opt.workers))
+    test(test_loader, 0, Hnet=Hnet, Rnet=Rnet, criterion=criterion)
     print(
-        "#################################################### validation begin ########################################################")
-    start_time = time.time()
-    Hnet.eval()
-    Rnet.eval()
-    Hlosses = AverageMeter()
-    Rlosses = AverageMeter()
-    for i, data in enumerate(val_loader, 0):
-        Hnet.zero_grad()
-        Rnet.zero_grad()
-        all_pics = data
-        this_batch_size = int(all_pics.size()[0] / 2)
-
-        cover_img = all_pics[0:this_batch_size, :, :, :]
-        secret_img = all_pics[this_batch_size:this_batch_size * 2, :, :, :]
-
-        concat_img = torch.cat([cover_img, secret_img], dim=1)
-
-        # 数据放入GPU
-        if opt.cuda:
-            cover_img = cover_img.cuda()
-            secret_img = secret_img.cuda()
-            concat_img = concat_img.cuda()
-
-        concat_imgv = Variable(concat_img, volatile=True)
-        cover_imgv = Variable(cover_img, volatile=True)
-
-        container_img = Hnet(concat_imgv)
-        errH = criterion(container_img, cover_imgv)
-        Hlosses.update(errH.data, this_batch_size)
-
-        rev_secret_img = Rnet(container_img)
-        secret_imgv = Variable(secret_img, volatile=True)
-        errR = criterion(rev_secret_img, secret_imgv)
-        Rlosses.update(errR.data[0], this_batch_size)
-
-        if i % 50 == 0:
-            save_result_pic(this_batch_size, cover_img, container_img.data, secret_img, rev_secret_img.data, epoch, i,
-                            opt.validationpics)
-
-    val_hloss = Hlosses.avg
-    val_rloss = Rlosses.avg
-    val_sumloss = val_hloss + opt.beta * val_rloss
-
-    val_time = time.time() - start_time
-    val_log = "validation[%d] val_Hloss = %.6f\t val_Rloss = %.6f\t val_Sumloss = %.6f\t validation time=%.2f" % (
-        epoch, val_hloss, val_rloss, val_sumloss, val_time)
-    print_log(val_log, logPath)
-
-    if not opt.debug:
-        writer.add_scalar('validation/H_loss_avg', Hlosses.avg, epoch)
-        writer.add_scalar('validation/R_loss_avg', Rlosses.avg, epoch)
-        writer.add_scalar('validation/sum_loss_avg', val_sumloss, epoch)
-
-    print(
-        "#################################################### validation end ########################################################")
-    return val_hloss, val_rloss, val_sumloss
+        "##################   test is completed  ######################")
 
 
 def test(test_loader, epoch, Hnet, Rnet, criterion):
@@ -355,11 +156,6 @@ def test(test_loader, epoch, Hnet, Rnet, criterion):
 
         # concat cover and original secret to get the concat_img with 6 channels
         concat_img = torch.cat([cover_img, secret_img], dim=1)
-
-        if opt.cuda:
-            cover_img = cover_img.cuda()
-            secret_img = secret_img.cuda()
-            concat_img = concat_img.cuda()
 
         concat_imgv = Variable(concat_img, volatile=True)  # concat_img as input of Hiding net
         cover_imgv = Variable(cover_img, volatile=True)  # cover_imgv as label of Hiding net
@@ -407,26 +203,8 @@ def print_log(log_info, log_path, console=True):
 
 # save result pics, coverImg filePath and secretImg filePath
 def save_result_pic(this_batch_size, originalLabelv, ContainerImg, secretLabelv, RevSecImg, epoch, i, save_path):
-    if not opt.debug:
-        
-        
-        originalFrames = originalLabelv.resize_(this_batch_size, 3, opt.imageSize, opt.imageSize)
-        containerFrames = ContainerImg.resize_(this_batch_size, 3, opt.imageSize, opt.imageSize)
-        secretFrames = secretLabelv.resize_(this_batch_size, 3, opt.imageSize, opt.imageSize)
-        revSecFrames = RevSecImg.resize_(this_batch_size, 3, opt.imageSize, opt.imageSize)     
-        
-        showContainer = torch.cat([originalFrames, containerFrames], 0)
-        showReveal = torch.cat([secretFrames, revSecFrames], 0)
-        # resultImg contains four rows: coverImg, containerImg, secretImg, RevSecImg, total this_batch_size columns
-        resultImg = torch.cat([showContainer, showReveal], 0)
-        resultImgName = '%s/ResultPics_epoch%03d_batch%04d.png' % (save_path, epoch, i)
-        vutils.save_image(resultImg, resultImgName, nrow=this_batch_size, padding=1, normalize=True)
-        
-        vutils.save_image(ContainerImg,'%s/cover_img.png' % save_path)
-        vutils.save_image(RevSecImg,'%s/RevSec_img.png' % save_path)
-
-        vutils.save_image(ContainerImg, '%s/Container.png' % opt.test)
-        vutils.save_image(RevSecImg, '%s/RevSec.png' % opt.test)
+    vutils.save_image(ContainerImg, '%s/Container.png' % opt.test)
+    vutils.save_image(RevSecImg, '%s/RevSec.png' % opt.test)
 
 
 class AverageMeter(object):
