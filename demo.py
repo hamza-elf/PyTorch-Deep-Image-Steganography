5422fff11545e85dbc29c2782daa958537c7210a
hamza-elf <elfaidihamza@gmail.com>
2021-06-10 18:47:41 +0100

    début interface graphique
    Co-Authored-By: AchrafDer <20994890+Melvero@users.noreply.github.com>

--- demo.py
+++ demo.py
@@ -4,17 +4,6 @@ import shutil
 import socket
 import time
 
-import torch
-import torch.backends.cudnn as cudnn
-import torch.nn as nn
-import torch.nn.parallel
-import torch.optim as optim
-import torch.utils.data
-import torchvision.utils as vutils
-from tensorboardX import SummaryWriter
-from torch.autograd import Variable
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torch.utils.data import DataLoader
 
 import utils.transformed as transforms
 from data.ImageFolderDataset import MyImageFolder
@@ -49,8 +38,6 @@ def process_images():
 # sg.theme('DarkAmber')   # Add a touch of color
 w, h = size_of_image = (250, 250)
 # All the stuff inside your window.
-layout_encoder = [[sg.Text('Réseau Codeur')],
-                  [sg.FileBrowse('Couverture'), sg.InputText(enable_events=True, key='-FCOUV-')],
                   [sg.Image(key='-IMCOUV-', size=(250, 250))],
                   [sg.FileBrowse('Secret'), sg.InputText(enable_events=True, key='-FSEC-')],
                   [sg.Image(key='-IMSEC-', size=(250, 250))],
@@ -58,9 +45,16 @@ layout_encoder = [[sg.Text('Réseau Codeur')],
                    sg.Button('Cancel')]]
 layout_decoder = [[sg.Text('Réseau Décodeur')],
                   [sg.Text('Image Container')],
+layout_text = [[sg.Image(filename='ensias.png', pad=((0,0),(0,450)))],
+               {sg.Text('Encadré par : \nPr.Abdelaziz S.DOUKKALI', justification='left', font=("Helvetica", 12),
+                        text_color='black')}]
+layout_decoder = [[sg.Text('Réseau Décodeur', font=("Helvetica", 25))],
+                  [sg.Text('Image Stego', font=("Helvetica", 16))],
                   [sg.Image(key='-IMCONTAINER-', size=(250, 250)),
                    sg.Button('Extraire', enable_events=True, key='-EXTRACT-',visible=False)],
                   [sg.Text('Image Secrète extraite')],
+                   sg.Button('Extraire', font=("Helvetica", 12), button_color=('','green'), enable_events=True, key='-EXTRACT-', visible=False)],
+                  [sg.Text('Image Secrète extraite',font=("Helvetica", 16))],
                   [sg.Image(key='-IMREVSEC-', size=(250, 250))]]
 layout = [[sg.Column(layout_encoder, pad=(0, 0)),
            sg.VSep(),
